82476d5c8c65b40613b4dd448a4546aaa14309a1
johame72 <johnhmerkel@gmail.com>
2023-10-05 09:26:49 -0500

    Map update

--- weather.js
+++ weather.js
@@ -13,25 +13,25 @@ fetch(endPoint)
         console.error("Error fetching the weather data:", error);
     });
 
-// ... [previous code] ...
-
 // Initialize the map
-const map = L.map('weatherMap').setView([30.3422, -97.7970], 5); // Austin, TX coordinates and zoom level
+const map = L.map('weatherMap').setView([30.3422, -97.7970], 10); // Zoomed into Austin
 
 // Add a base map layer (for example, OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// Add the temperature layer
-const temperatureLayer = `https://maps.openweathermap.org/maps/2.0/weather/1h/TA2/{z}/{x}/{y}?appid=5ad4601afb2e22f1d595b000914778bb`;
-L.tileLayer(temperatureLayer, {
-    opacity: 0.5,
-    zIndex: 1
-}).addTo(map);
+// Temperature layer
+const temperatureLayer = L.tileLayer('https://maps.openweathermap.org/maps/2.0/weather/1h/TA2/{z}/{x}/{y}?appid=5ad4601afb2e22f1d595b000914778bb');
 
-// Add the precipitation layer
-const precipitationLayer = `https://maps.openweathermap.org/maps/2.0/weather/1h/PR0/{z}/{x}/{y}?appid=5ad4601afb2e22f1d595b000914778bb`;
-L.tileLayer(precipitationLayer, {
-    opacity: 0.5,
-    zIndex: 2
-}).addTo(map);
+// Precipitation layer
+const precipitationLayer = L.tileLayer('https://maps.openweathermap.org/maps/2.0/weather/1h/PR0/{z}/{x}/{y}?appid=5ad4601afb2e22f1d595b000914778bb');
 
+// Layer control
+const baseLayers = {
+    "Temperature": temperatureLayer,
+    "Precipitation": precipitationLayer
+};
+
+L.control.layers(baseLayers).addTo(map);
+
+// Default layer
+temperatureLayer.addTo(map);
