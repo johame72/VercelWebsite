90d8db267024ee7061c0329159e00b989cf94c92
johame72 <johnhmerkel@gmail.com>
2023-10-08 10:29:44 -0500

    Added Figma site info

---
+++ index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Rectangle } from "./Rectangle";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="overlap">
+        <div className="div" />
+        <div className="rectangle-2" />
+        <div className="rectangle-3" />
+        <div className="rectangle-4" />
+        <div className="text-wrapper">ChatFormat</div>
+      </div>
+      <div className="overlap-group">
+        <div className="overlap-2">
+          <div className="overlap-group-2">
+            <div className="rectangle-5" />
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="rectangle-8" />
+            <div className="rectangle-9" />
+            <div className="rectangle-10" />
+            <div className="rectangle-11" />
+            <div className="rectangle-12" />
+          </div>
+          <div className="rectangle-13" />
+          <div className="rectangle-14" />
+        </div>
+        <div className="rectangle-15" />
+      </div>
+      <Rectangle className="rectangle-instance" />
+      <div className="rectangle-16" />
+    </div>
+  );
+};
--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
-const apiKey = '5ad4601afb2e22f1d595b000914778bb';
 const lat = 30.3422;
 const lon = -97.7970;
-const endPoint = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&units=imperial&appid=${apiKey}`;
 
-fetch(endPoint)
+// Update this to point to your backend endpoint
+const backendEndPoint = `/weather?lat=${lat}&lon=${lon}`;
+
+fetch(backendEndPoint)
     .then(response => response.json())
     .then(data => {
         document.getElementById('temperature').innerText = `Temperature: ${data.current.temp}°F`;
@@ -19,11 +20,9 @@ const map = L.map('weatherMap').setView([30.3422, -97.7970], 10); // Zoomed into
 // Add a base map layer (for example, OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// Temperature layer
-const temperatureLayer = L.tileLayer('https://maps.openweathermap.org/maps/2.0/weather/1h/TA2/{z}/{x}/{y}?appid=5ad4601afb2e22f1d595b000914778bb');
-
-// Precipitation layer
-const precipitationLayer = L.tileLayer('https://maps.openweathermap.org/maps/2.0/weather/1h/PR0/{z}/{x}/{y}?appid=5ad4601afb2e22f1d595b000914778bb');
+// Update these to use your backend server as a proxy if needed
+const temperatureLayer = L.tileLayer('https://maps.openweathermap.org/maps/2.0/weather/1h/TA2/{z}/{x}/{y}');
+const precipitationLayer = L.tileLayer('https://maps.openweathermap.org/maps/2.0/weather/1h/PR0/{z}/{x}/{y}');
 
 // Layer control
 const baseLayers = {
